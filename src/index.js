6012a55897a19626f4e4081af8d56293444f2178
tanopanta <25007602+tanopanta@users.noreply.github.com>
2021-09-03 18:19:36 +0900

    add button

---
+++ src/index.js
@@ -0,0 +1,34 @@
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+  
+  
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  document.getElementById("incomplete-list").appendChild(div);
+}
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
