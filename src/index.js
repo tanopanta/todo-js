178f645a4dc506d9bf6fffebcc56b84d25b81701
tanopanta <25007602+tanopanta@users.noreply.github.com>
2021-09-03 18:30:33 +0900

    add delete button

--- src/index.js
+++ src/index.js
@@ -12,13 +12,15 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
+    
     alert("完了");
   });
 
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert("削除");
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
   });
   
   
